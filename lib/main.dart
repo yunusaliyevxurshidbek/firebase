6fe5a5c4b64f56484986978d1597524da8f25ba9
Xurshidbek <xurshidbek4466@gmail.com>
2024-11-14 19:09:29 +0500

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
 
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        centerTitle: true,
+      ),
+    );
   }
 }
